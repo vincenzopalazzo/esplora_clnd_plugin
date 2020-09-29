3a1349d345592e448163ebe919b70b7fbe9aa9a4
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2020-09-30 00:29:37 +0200

    Fixed bug parsing configuration inside init method
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- esplora.c
+++ esplora.c
@@ -662,14 +662,14 @@ static bool configure_esplora_with_network(const char *network,
 
 static void init(struct plugin *p, const char *buffer, const jsmntok_t *config)
 {
-	const jsmntok_t *proxy_tok = json_get_member(buffer, config, "proxy");
-	if (proxy_tok) {
+  const jsmntok_t *proxy_tok = json_get_member(buffer, config, "proxy");
+  if (proxy_tok) {
 		const jsmntok_t *address_tok =
-		    json_get_member(buffer, config, "address");
+		    json_get_member(buffer, proxy_tok, "address");
 		const jsmntok_t *port_tok =
-		    json_get_member(buffer, config, "port");
+		    json_get_member(buffer, proxy_tok, "port");
 		const jsmntok_t *torv3_tok =
-		    json_get_member(buffer, config, "torv3-enabled");
+      json_get_member(buffer, config, "torv3-enabled");
 		const jsmntok_t *always_proxy =
 		    json_get_member(buffer, config, "use_proxy_always");
 		if (address_tok && port_tok && torv3_tok && always_proxy) {
